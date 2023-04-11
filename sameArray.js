ff1d2da583a20e9fce094e304f27a52a351f0881
Tamilarasan Pannerselvam <tamilarasan@ideas2it.com>
2023-04-11 17:32:10 +0530

    New logics

---
+++ sameArray.js
@@ -0,0 +1,41 @@
+function same(arr1, arr2) {
+    //console.time("rr")
+    if (arr1.length !== arr2.length) {
+      return false
+    }
+    
+    let temp = {};
+    let square;
+    for (let i=0; i<arr1.length; i++) {
+        square = arr1[i] * arr1[i];
+        if (temp[square]) {
+            temp[square] = temp[square] + 1
+        } else {
+            temp[square] = 1;
+        }
+    //   temp  = temp + ( arr1[i] * arr1[i] - arr2[i])
+    //   console.log(temp)
+      // index = arr2.indexOf(arr1[i] * arr1[i]);
+      // if( index === -1) {
+      //   console.timeEnd("rr")
+      //   return false;
+      // }
+      
+      // arr2.splice(index ,1);
+    }
+    
+    for(let i=0; i<arr2.length;i++) {
+      if (temp[arr2[i]] && temp[arr2[i]] > 0) {
+        temp[arr2[i]] = temp[arr2[i]] -1;
+      } else {
+        return false
+      }
+    }
+    
+    //console.timeEnd("rr")
+    return true
+}
+  
+console.log(same([3,4,4,0], [16,16,9,0])) // true
+console.log(same([1,1], [1])) // false
+console.log(same([1,2,4], [16,1,4])) // true
---
+++ twoSum.js
@@ -0,0 +1,49 @@
+arr = [1,5,7,9,10,17,21];
+tar = 16;
+
+function twoSum(arr, target) {
+  let min = 0, max = arr.length - 1;
+  while(min !== max) {
+   
+    let sum = arr[min] + arr[max];
+    if (sum > target) {
+      max --;
+    } else if (sum<target) {
+      min++
+    } else {
+      return true
+    }
+  }
+  
+  return false;
+}
+
+console.log(twoSum(arr, 16)) // True
+console.log(twoSum(arr, 13)) // False
+console.log(twoSum(arr, 22)) // true
+console.log(twoSum(arr, 27)) // true
+
+
+arr = [9,7,10,1,17,5,21];
+tar = 16;
+
+function twoSumUnOrdered(arr, target) {
+  let temp = {};
+  
+  for(i=0;i<arr.length; i++) {
+    let rem = target - arr[i];
+    if (temp[rem]) {
+      console.log(arr[i], rem) // 9,7 , 21,1 , 17,10
+      return true
+    } else {
+      temp[arr[i]] = rem;  
+    } 
+  }
+  console.log(-1,-1)
+  return false
+} 
+
+console.log(twoSumUnOrdered(arr, 16)) // true
+console.log(twoSumUnOrdered(arr, 13)) // false
+console.log(twoSumUnOrdered(arr, 22)) // true
+console.log(twoSumUnOrdered(arr, 27)) // false
